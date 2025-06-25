2503cc7262e61b92f50c52b0dd53f7041f87e4e8
Guilherme Calheiros <guilhermecalheiros522@gmail.com>
2025-06-25 17:36:59 -0300

    removendo páginas desnecessárias e comentando rotas que não estão sendo utilizadas

--- resources/js/Pages/Test.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-const Test = () => {
-    return (
-        <h1 className="text-3xl font-bold text-blue-500">Ola mundo...</h1>
-    )
-};
- 
-export default Test
